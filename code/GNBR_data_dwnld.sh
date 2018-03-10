6f88443364822ea99f8f00c21cb77d6919aa1563
alavertu <adamlavertu@gmail.com>
2018-03-10 12:16:57 -0800

    script with wget for GNBR files

---
+++ code/GNBR_data_dwnld.sh
@@ -0,0 +1,25 @@
+# Download GNBR theme data and entity mappings
+
+wget https://zenodo.org/record/1134693/files/part-i-chemical-disease-path-theme-distributions.txt
+
+wget https://zenodo.org/record/1134693/files/part-i-chemical-gene-path-theme-distributions.txt
+
+wget https://zenodo.org/record/1134693/files/part-i-gene-disease-path-theme-distributions.txt
+
+wget https://zenodo.org/record/1134693/files/part-i-gene-gene-path-theme-distributions.txt
+
+wget https://zenodo.org/record/1134693/files/part-ii-dependency-paths-chemical-disease-sorted-with-themes.txt
+
+wget https://zenodo.org/record/1134693/files/part-ii-dependency-paths-chemical-gene-sorted-with-themes.txt
+
+
+# wget https://zenodo.org/record/1134693/files/part-ii-dependency-paths-gene-disease-sorted-with-themes.txt
+
+wget https://zenodo.org/record/1134693/files/part-ii-dependency-paths-gene-gene-sorted-with-themes.txt 
+
+
+
+# Download CTD data
+
+# From here
+http://ctd.mdibl.org/downloads/
